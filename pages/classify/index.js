0898cbf33b1c64a0baeb1fce2923c5de4dc937e1
MadMay <1054716511@qq.com>
2020-06-05 00:21:59 +0800

    分类页电影区

--- pages/classify/index.js
+++ pages/classify/index.js
@@ -1,18 +1,87 @@
 // pages/classify/index.js
+import{request} from '../../utils/request';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    active:0,
+    love:'爱情电影',
+    scienc:'科幻电影',
+    terror:'恐怖电影',
+    strange:'奇幻电影',
+    feature:'剧情电影',
+    fightFilm:[],//动作
+    comedyFilm:[],//喜剧
+    loveFilm:[],//爱情
+    sciencFilm:[],//科幻
+    terrorFilm:[],//恐怖
+    strangeFilm:[],//奇幻
+    featureFilm:[],//剧情
   },
-
+  
   /**
    * 生命周期函数--监听页面加载
    */
+  //获取动作电影数据
+  async getFilmData(){
+    const {data} =  await request({query:"?cid=5"});
+    this.setData({
+      fightFilm:data,
+      fight: "动作电影",
+    })
+  },
+  async getFilmData1(){
+    const {data} =  await request({query:"?cid=6"});
+    this.setData({
+      comedyFilm:data,
+      comedy:"喜剧电影",
+    })
+  },
+  async getFilmData2(){
+    const {data} =  await request({query:"?cid=7"});
+    this.setData({
+      loveFilm:data,
+      love:"爱情电影",
+    })
+  },
+  async getFilmData3(){
+    const {data} =  await request({query:"?cid=8"});
+    this.setData({
+      sciencFilm:data,
+      scienc:"科幻电影",
+    })
+  },
+  async getFilmData4(){
+    const {data} =  await request({query:"?cid=9"});
+    this.setData({
+      terrorFilm:data,
+      terror:"恐怖电影",
+    })
+  },
+  async getFilmData5(){
+    const {data} =  await request({query:"?cid=10"});
+    this.setData({
+      strangeFilm:data,
+      strange:"奇幻电影",
+    })
+  },
+  async getFilmData6(){
+    const {data} =  await request({query:"?cid=11"});
+    this.setData({
+      featureFilm:data,
+      feature:"喜剧电影",
+    })
+  },
   onLoad: function (options) {
-
+    this.getFilmData();
+    this.getFilmData1()
+    this.getFilmData2()
+    this.getFilmData3()
+    this.getFilmData4()
+    this.getFilmData5()
+    this.getFilmData6()
   },
 
   /**
@@ -62,5 +131,8 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  doSomeThing(e){
+    console.log("滑动...",e)
   }
 })
